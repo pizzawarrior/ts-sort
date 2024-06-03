9b1ce4d758d63c09846460af887fab3c5486a1ad
pizzawarrior <i.norstad@gmail.com>
2024-06-03 16:14:46 -0700

    project complete, may add tests later

--- src/index.ts
+++ src/index.ts
@@ -2,13 +2,13 @@ import { NumbersCollection } from "./NumbersCollection";
 import { CharactersCollection } from "./CharactersCollection";
 import { LinkedList } from "./LinkedList";
 
-// const numbersCollection = new NumbersCollection([15, 45, 765, -17, 12]);
-// numbersCollection.sort();
-// console.log(numbersCollection.data);
+const numbersCollection = new NumbersCollection([15, 45, 765, -17, 12]);
+numbersCollection.sort();
+console.log(`Sorted numbers from the numbers collection: ${numbersCollection.data}`);
 
-// const charCollection = new CharactersCollection('zebraliXcious')
-// charCollection.sort()
-// console.log(charCollection.data)
+const charCollection = new CharactersCollection('zebraliXcious')
+charCollection.sort()
+console.log(`Sorted chars from the char collection: ${charCollection.data}`)
 
 const linkedList = new LinkedList()
 linkedList.add(-40)
@@ -16,4 +16,5 @@ linkedList.add(13)
 linkedList.add(7)
 linkedList.add(8)
 linkedList.sort();
+console.log('Sorted numbers from the linked list:')
 linkedList.print();
